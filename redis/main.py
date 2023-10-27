11cf35a0edfc113b41c285cd967cc5189a9ff3c9
Majestic Coding <33904170+mattmajestic@users.noreply.github.com>
2023-10-27 11:51:42 -0400

    update redis

--- redis/main.py
+++ redis/main.py
@@ -31,5 +31,5 @@ async def read_redis_key(key: str):
     except Exception as e:
         return {"error": f"Failed to connect to Redis: {e}"}
 
-if __name__ == "__redis__":
+if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8883)
